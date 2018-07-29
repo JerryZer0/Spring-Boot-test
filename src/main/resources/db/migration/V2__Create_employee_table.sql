3f804476a33c0c2f7aadf984d19ef7336a9fcecf
Jerry <jerry.zhang@oocl.com>
2018-07-29 17:15:09 +0800

    [Jerry]employeeRepositoryTest change get add and pass all

--- src/main/resources/db/migration/V2__Create_employee_table.sql
+++ src/main/resources/db/migration/V2__Create_employee_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(32) NOT NULL,
     age INT NOT NULL,
-    gender VARCHAR(4) NOT NULL,
+    gender VARCHAR(10) NOT NULL,
     company_id INT
 )
--- src/test/java/com/oocl/RestfulAPI/entities/EmployeeRepositoryTest.java
+++ src/test/java/com/oocl/RestfulAPI/entities/EmployeeRepositoryTest.java
@@ -21,7 +21,7 @@ import static org.junit.Assert.*;
 public class EmployeeRepositoryTest {
 
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private EmployeeRepository repository;
 
     @Autowired
     private TestEntityManager manager;
@@ -32,17 +32,77 @@ public class EmployeeRepositoryTest {
     }
 
     @Test
+    public void should_add_employee_successfully(){
+        //given
+        Employee employee = new Employee("张三",28,"male",new Company("kuaile"));
+        //when
+        repository.save(employee);
+        //then
+        assertThat(repository.findAll().size(),is(1));
+    }
+
+    @Test
     public void should_return_all_employees(){
         //given
         manager.persist(new Employee("张三",28,"male",new Company("kuaile")));
         manager.persist(new Employee("Joker",18,"male",new Company("moments")));
         //when
-        List<Employee> employees = employeeRepository.findAll(PageRequest.of(0,5)).getContent();
+        List<Employee> employees = repository.findAll(PageRequest.of(0,5)).getContent();
 
         //then
+        System.err.println("654654dfgdf "+employees.size());
         assertThat(employees.size(), is(2));
         assertThat(employees.get(0).getName(), is("张三"));
         assertThat(employees.get(0).getAge(), is(28));
     }
+    
+    @Test
+    public void should_return_the_employee_with_id_is_1(){
+        //given
+        int id = Integer.valueOf(manager.persistAndGetId(new Employee("张三",18,"male",new Company("hah"))).toString());
+        manager.persist(new Employee("sisi",18,"female",new Company("hah ")));
+        //when
+        Employee employee = repository.findById(id).get();
+        //then
+        //assertThat(employee.getId(),is(1));
+        assertThat(employee.getAge(),is(18));
+        assertThat(employee.getName(),is("张三"));
+        assertThat(employee.getCompany().getName(),is("hah"));
+    }
 
+    @Test
+    public void should_delete_employee_by_entity_successfully(){
+        //given
+        manager.persist(new Employee("张三",18,"male",new Company("hah")));
+        Employee employee = manager.persistAndFlush(new Employee("sisi",18,"female",new Company("hah ")));
+        //when
+        repository.delete(employee);
+        //then
+        assertThat(repository.findAll().size(),is(1));
+    }
+
+    @Test
+    public void should_delete_employee_by_id_successfully(){
+        //given
+        //manager.clear();
+        int idBefor = Integer.valueOf(manager.persistAndGetId(new Employee("张三",18,"male",new Company("hah"))).toString());
+        //when
+        Integer id = Integer.valueOf(manager.persistAndGetId(new Employee("sisi",18,"female",new Company("hah "))).toString());
+        repository.deleteById(id);
+        //then
+        assertThat(id,is(idBefor+1));
+        assertThat(repository.findAll().size(),is(1));
+    }
+    
+    @Test
+    public void should_update_info_successfully(){
+        //given
+        int id = Integer.valueOf(manager.persistAndGetId(new Employee("张三",18,"male",new Company("hah"))).toString());
+        Employee employee = new Employee("Joker",20,"male",new Company("hah"));
+        employee.setId(id);
+        //when
+        repository.save(employee);
+        //then
+        assertThat(repository.findById(id).get().getName(),is("Joker"));
+    }
 }
